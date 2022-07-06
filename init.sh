955060d10d94755372a9139b6f9fd274fb492547
Sebastien Penet <sebastine@pe-net.fr>
2022-07-06 06:04:34 +0000

    Add git initialisation in init.sh

--- init.sh
+++ init.sh
@@ -4,3 +4,9 @@ export AZURE_TENANT_ID="b1d48f7c-fcc5-4713-8c1e-bcfb9952c5ba"
 export KEY_VAULT_NAME=uploadfiles
 export FLASK_APP=uploadFiles.py
 export FLASK_ENV=development
+
+git config user.email "sebastine@pe-net.fr"
+git config user.name "Sebastien Penet"
+git branch -M main
+git remote add origin https://github.com/marlenagui/uploadFiles.git
+git push -u origin main
